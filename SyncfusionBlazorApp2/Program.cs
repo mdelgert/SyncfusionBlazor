fb71f7327b49b9ebe3d21d08d2d151c29dd9de0f
Matthew David Elgert <mdelgert@yahoo.com>
2025-03-03 15:53:53 -0500

    Add Syncfusion Blazor App 2 project structure and initial components

---
+++ SyncfusionBlazorApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Blazor;
+using SyncfusionBlazorApp2;
+using SyncfusionBlazorApp2.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+var licenseKey = configuration["SyncfusionLicensing"];
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
